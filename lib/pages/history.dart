f947911a73ba9cca337af86986d3eb661dedbe6c
romil_shah <romilshah3011@gmail.com>
2024-03-30 01:08:44 +0530

    history change

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
+//History Page
 class History extends StatefulWidget {
   const History({super.key});
 
